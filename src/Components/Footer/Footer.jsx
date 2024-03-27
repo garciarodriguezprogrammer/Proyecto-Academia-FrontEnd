e7236e534e8fbcb5f50d1a795cd5f90269a3f8c7
Dayanis <dayany1588@yahoo.com>
2024-03-27 04:02:38 +0100

    Definido el contenido del footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,23 @@
 import React from "react";
- import "./Footer.css"
+import "./Footer.css"
 
 
 export const Footer = () => {
     return (
         <div className="footer-container">
             {/* Contenido de tu aplicación */}
-            <footer className="py-3 my-4" style={{ backgroundColor: "#f8f8f8", borderTop: "1px solid #ddd" }}>
+            <footer className="py-3 my-4" style={{ backgroundColor: "#f8f8f80", borderTop: "1px solid #ddd" }}>
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Próximos proyectos:</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Clases de flamenco</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Clases de rumba</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Clases de rock&roll</a></li>
                 </ul>
-                <p className="text-center text-body-secondary">&copy; 2024 Company, Inc</p>
+                <div className="text-center">
+                    <span className="brand-text-all logo">All</span>
+                    <span className="brand-text-number logo">4</span>
+                    <span className="brand-text-dancing logo">dancing</span>
+                </div>
             </footer>
         </div>
     )
